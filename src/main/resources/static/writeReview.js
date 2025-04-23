f64ce690f6ea1a1afbcdcc9298c26a165b13ed86
kjsod <kjsod9908@oslomet.no>
2025-04-23 20:50:18 +0200

    form validation

--- src/main/resources/static/writeReview.js
+++ src/main/resources/static/writeReview.js
@@ -18,34 +18,33 @@ $(document).ready(function () {
     form.submit(function(e) {
         e.preventDefault();
 
-        console.log($('#review'));
-        console.log($('#review').val());
+        if ($('#review').val().trim() === "" || $('#stars').val() === "") {
+            alert("Fields cannot be empty");
+        } else {
+            const review = {
+                review: $('#review').val(),
+                stars: $('#stars').val(),
+                productid: $('#selectProduct').val()
+            };
 
-        const review = {
-            review: $('#review').val(),
-            stars: $('#stars').val(),
-            productid: $('#selectProduct').val()
-        };
-
-        fetch("http://localhost:8080/api/reviews", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(review)
-        })
-            .then(res => {
-                res.text();
+            fetch("http://localhost:8080/api/reviews", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(review)
             })
-            .then(data => {
-                console.log('Review opprettet',data);
-                alert("Review successfully created");
-                window.location.href=`/reviews?id=${$('#selectProduct').val()}`;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-
-    })
-
+                .then(res => {
+                    res.text();
+                })
+                .then(data => {
+                    console.log('Review opprettet', data);
+                    alert("Review successfully created");
+                    window.location.href = `/reviews?id=${$('#selectProduct').val()}`;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    });
 });
